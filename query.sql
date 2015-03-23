c0a4448e68d78cb1a4e5f95a28e3be4259dbdfc9
huangxiaobin <huangxb_bj@126.com>
2015-03-23 13:53:41 +0100

    tutorial 11-19

--- query.sql
+++ query.sql
@@ -29,3 +29,50 @@ SELECT id,name FROM customers WHERE id!=54;
 SELECT id,name FROM customers WHERE id<7;
 SELECT id,name FROM customers WHERE id BETWEEN 25 AND 30;
 SELECT name,state FROM customers WHERE state='CA';
+
+--t11
+SELECT name,state,city FROM customers WHERE state='CA' AND city='Hollywood';
+SELECT name,state,city FROM customers WHERE state='CA' OR city='Boston';
+SELECT id, name,city FROM customers WHERE (id=1 OR id=2) AND city='Raleigh';
+
+--t12
+SELECT name,state FROM customers WHERE state='CA' OR state='NC' OR state='NY';
+SELECT name,state FROM customers WHERE state NOT IN ('CA', 'NC','NY') ORDER BY state;
+
+--t13
+SELECT name FROM items WHERE name LIKE 'new%';
+SELECT name FROM items WHERE name LIKE '%computer%';
+SELECT city FROM customers WHERE city LIKE 'h%d';
+
+--t14
+SELECT name FROM items WHERE name LIKE '_ boxes of frogs';
+SELECT name FROM items WHERE name LIKE '% boxes of frogs';
+
+--t15
+SELECT name FROM items WHERE name REGEXP 'new';
+SELECT name FROM items WHERE name REGEXP '.boxes';
+SELECT name FROM items WHERE name REGEXP 'car|gold';
+SELECT name FROM items WHERE name REGEXP '[12345] boxes of frogs';
+SELECT name FROM items WHERE name REGEXP '[^12345] boxes of frogs';
+SELECT name FROM items WHERE name REGEXP '[1-5] boxes of frogs';
+
+--t16
+SELECT CONCAT(city, ', ', state) FROM customers;
+SELECT CONCAT(city, ', ', state) AS new_address FROM customers;
+SELECT name, cost, cost-1 AS sale_price FROM items;
+
+--t17
+SELECT name, UPPER(name) FROM customers;
+SELECT cost, SQRT(cost) FROM items;
+SELECT AVG(cost) FROM items;
+SELECT SUM(bids) FROM items;
+
+--t18
+SELECT COUNT(name) FROM items WHERE seller_id=6;
+SELECT AVG(name) FROM items WHERE seller_id=6;
+SELECT COUNT(*) AS item_count, MAX(cost) AS max_cost, AVG(cost) AS avg_cost FROM items WHERE seller_id=12;
+
+--t19
+SELECT seller_id, COUNT(*) AS item_count FROM items WHERE seller_id=1;
+SELECT seller_id, COUNT(*) AS item_count FROM items GROUP BY seller_id;
+SELECT seller_id, COUNT(*) AS item_count FROM items GROUP BY seller_id HAVING COUNT(*)>3 ORDER BY item_count DESC;
